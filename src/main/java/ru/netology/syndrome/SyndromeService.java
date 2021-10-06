938c2a0c35b59395e64ac28b215eef53ea414b20
Tukayama <ya.tumanov2009@yandex.ru>
2021-10-06 17:59:29 +0300

    init commit

--- src/main/java/ru/netology/syndrome/SyndromeService.java
+++ src/main/java/ru/netology/syndrome/SyndromeService.java
@@ -11,7 +11,7 @@ public class SyndromeService {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
-            if (current_max < income)
+            if (current_max<=income)
                 current_max = income;
         return current_max;
     }
