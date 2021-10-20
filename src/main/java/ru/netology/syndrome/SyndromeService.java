3fc74127a78787710f7fd284780169d1c3d27c41
Tukayama <ya.tumanov2009@yandex.ru>
2021-10-20 15:38:13 +0300

    init commit

--- src/main/java/ru/netology/syndrome/SyndromeService.java
+++ src/main/java/ru/netology/syndrome/SyndromeService.java
@@ -11,9 +11,16 @@ public class SyndromeService {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes)
-            if (current_max<=income)
+            if (current_max < income) {
                 current_max = income;
+            }
         return current_max;
+
     }
+
+
 }
+
+
+
 
--- src/test/java/ru/netology/syndrome/SyndromeServiceTest.java
+++ src/test/java/ru/netology/syndrome/SyndromeServiceTest.java
@@ -10,12 +10,10 @@ class SyndromeServiceTest {
     void findMax() {
         SyndromeService service = new SyndromeService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {7, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
-
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
     }
+
 }
